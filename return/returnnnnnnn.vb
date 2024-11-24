8c0a069512d34877419b4d262e32949cf65cea1d
Steam <15368104568@189.cn>
2024-11-24 13:46:55 +0800

    Update returnnnnnnn.vb

--- return/returnnnnnnn.vb
+++ return/returnnnnnnn.vb
@@ -3,11 +3,11 @@ ReDim t11 As Async
 ReDim t0 As Await
 Function t11t0
      t11[
-       'w ^ exp(0 + t5)' + 'x ^ log(0 - t6)',
-       'y ^ exp(0 - t5)' + 'z ^ log(0 + t6)'
+       'w ^ exp(0 + t11)' + 'x ^ log(0 - t0)',
+       'y ^ exp(0 - t11)' + 'z ^ log(0 + t0)'
     ]
      t0[
-       'w ^ exp(www + t5)' + 'x ^ log(www - t6)',
-       'y ^ exp(www - t5)' + 'z ^ log(www + t6)'
+       'w ^ exp(www + t11)' + 'x ^ log(www - t0)',
+       'y ^ exp(www - t11)' + 'z ^ log(www + t0)'
     ]
 End Function
