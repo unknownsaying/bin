6750f842723bf5e1abe1ac978119c5bcf47d0747
whitespacescape <15368104568@189.cn>
2024-12-14 10:01:09 +0800

    Update RGBimpact.vb

--- tensor/color/RGBimpact.vb
+++ tensor/color/RGBimpact.vb
@@ -5,7 +5,7 @@ Imports './anti.vb
 const impact0 = 'element1 + element2 + element3 + element4'
 const impact1 = 'element5 - element6 - element7 - element8'
 const impact2 = 'element9 * element10 * element11 *element12'
-const impact3 = 'element13 / element14 / element15 /  element16'
+const impact3 = 'element13 / element14 / element15 / element16'
 const impact4 = 'element17 % element18 % element19 % element20'
 
 const impact5 = 'impact0 * impact1' + 'impact2 * impact3'
