c3f8842a499f27fbf32ab38dd6a1b17da2fa01e4
unknownsaying <15368104568@189.cn>
2025-01-31 00:12:33 +0000

    Update x7y7z7t7z7y7x7.vb

--- XYZT/x7y7z7t7z7y7x7.vb
+++ XYZT/x7y7z7t7z7y7x7.vb
@@ -4,7 +4,7 @@ Imports '../XYZT/x3y3z3t3'
 Imports '../XYZT/t4z4y4x4'
 Imports '../XYZT/t5z5y5x5'
 Imports '../XYZT/t6z6y6x6'
-function returnnnnnnnn(x7y7z7t7z7y7x7:any){
+function returnnnnnnnn(x7y7z7t7z7y7x7){
     function returnn() {
         return x1y1z1t1
     }
